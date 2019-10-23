d6c60bb6061632cc0742a53c655e037c005ce9d2
Jacek <jacek.mstowski@gmail.com>
2019-10-23 16:01:12 +0200

    Improved version(extra white space's are deleted) and commentary

--- parse_function.py
+++ parse_function.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def parse_function(name):
@@ -6,16 +7,20 @@ def parse_function(name):
     names_splitted = []
 
     if os.path.isfile(name):
-        with open(name) as file:
+        with open(name) as file:  # there is no need to close the file. "with" statement do this by itself
             for x in file:
                 if "import " in x:
                     names.append(x)
         for x in names:
-            words = x.split(" ")
+            re.sub('\s+', ' ', x).strip()  # Handling extra space's in line
+            words = x.split()  # Dividing line into words(creating a list o words)
 
-            names_splitted.append(words[1])
+            names_splitted.append(words[1])  # adding only file name to main list
 
         return names_splitted
     else:
         print("This is not a File.")
         return []
+
+
+
