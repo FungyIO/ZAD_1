5cc2a2250f6bd00c940c5e74bf28c99ee2991585
Jacek <jacek.mstowski@gmail.com>
2019-10-23 18:01:23 +0200

    Fixed error with "import "(probably)

--- parse_function.py
+++ parse_function.py
@@ -9,18 +9,20 @@ def parse_function(name):
     if os.path.isfile(name):
         with open(name) as file:  # there is no need to close the file. "with" statement do this by itself
             for x in file:
-                if "import " in x:
+                re.sub('\s+', ' ', x).strip()  # Handling extra space's in line
+                if x.startswith("import"):
                     names.append(x)
         for x in names:
-            re.sub('\s+', ' ', x).strip()  # Handling extra space's in line
-            words = x.split()  # Dividing line into words(creating a list o words)
 
+            words = x.split()  # Dividing line into words(creating a list o words)
             names_splitted.append(words[1])  # adding only file name to main list
+
 
         return names_splitted
     else:
         print("This is not a File.")
         return []
 
-
+b=parse_function("parse_function.py")
+print(b)
 
