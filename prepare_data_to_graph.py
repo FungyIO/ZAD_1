10ca1349d3e30cab006822599c53c5e76f4bb3fb
WojtekV <wpawlik123@gmail.com>
2019-11-07 16:26:07 +0100

    Changed ploting lib from networkx to graphvis

--- prepare_data_to_graph.py
+++ prepare_data_to_graph.py
@@ -1,16 +1,19 @@
 import networkx as nx
 import parse_filenames
+from graphviz import Digraph
 import os
 
  
 def prepare_data_to_graph(filenames):
-    G = nx.DiGraph()
+    G = Digraph('unix', filename='unix.gv',
+                node_attr={'color': 'lightblue2', 'style': 'filled'})
+    G.attr(size='6,6')
  
     for node in filenames:
         edges = parse_filenames.parse_function(node, filenames)
 
         for edge in edges:
-            nodesize = os.path.getsize(node)
-            G.add_edge(edge, node + "\n" + str(nodesize))
+            node_size = os.path.getsize(node)
+            G.edge(edge, node + "\n" + str(node_size))
     return G
 
