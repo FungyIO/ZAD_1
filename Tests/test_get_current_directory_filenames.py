5857362881799acda47a3e05b9b31937b98b0fbc
WojtekV <wpawlik123@gmail.com>
2019-12-12 13:41:10 +0100

    Added test for get_current_directory_filenames.py

---
+++ Tests/test_get_current_directory_filenames.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import directory_filenames
+
+
+class test_get_current_directory_filenames(TestCase):
+    def test_get_current_directory_filenames(self):
+        list_of_files = directory_filenames.get_current_directory_filenames("test_folder")
+        self.assertEqual(sorted(list_of_files), sorted(['file_1.py', 'file_2.py']))
