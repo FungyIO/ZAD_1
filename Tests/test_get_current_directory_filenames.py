039fde1736956d40bb31d1009156bc523b938bb9
WojtekV <wpawlik123@gmail.com>
2019-12-12 14:05:45 +0100

    Fixed bug with empty list in parse_functions_names and changed default argument in directory_filenames.py

--- Tests/test_get_current_directory_filenames.py
+++ Tests/test_get_current_directory_filenames.py
@@ -7,5 +7,5 @@ import directory_filenames
 
 class test_get_current_directory_filenames(TestCase):
     def test_get_current_directory_filenames(self):
-        list_of_files = directory_filenames.get_current_directory_filenames("test_folder")
+        list_of_files = directory_filenames.get_current_directory_filenames("/test_folder")
         self.assertEqual(sorted(list_of_files), sorted(['file_1.py', 'file_2.py']))
--- directory_filenames.py
+++ directory_filenames.py
@@ -4,7 +4,7 @@ import os
 def get_current_directory_filenames(dir=""):
     filenames = []
 
-    for root, dirs, files in os.walk("./" + dir):
+    for root, dirs, files in os.walk("." + dir):
         for filename in files:
             if filename[-3:] == '.py':
                 filenames.append(filename)
