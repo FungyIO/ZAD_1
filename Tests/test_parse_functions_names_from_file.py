aeb26351ca18342bdaf18c1ad05e5264e780865e
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2019-12-12 16:57:17 +0100

    updated parse_functions_names_from_file tests

--- Tests/test_parse_functions_names_from_file.py
+++ Tests/test_parse_functions_names_from_file.py
@@ -5,4 +5,4 @@ import parse_functions_names
 class TestFrom_file(unittest.TestCase):
     def test_parse_functions_names_from_file(self):
         list_of_functions = parse_functions_names.from_file('test_file.py')
-        self.assertEqual(list_of_functions, ['funkcja1', 'funkcja2', 'funkcja3', 'funkcja4'])
+        self.assertEqual(sorted(list_of_functions), sorted(['funkcja1', 'funkcja2', 'funkcja3', 'funkcja4']))
