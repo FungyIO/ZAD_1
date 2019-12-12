a0880b47aa87802213f8c041438e5c954418307f
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2019-12-12 08:54:24 +0100

    tests added

--- functions_dependency.py
+++ functions_dependency.py
@@ -2,14 +2,15 @@ import parse_functions_names
 import directory_filenames
 import function_parser
 from graphviz import Digraph
-
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Users/Lenovo/AppData/Local/graphviz-2.38/release/bin'
 
 def functions_dependency():
     functions_list = parse_functions_names.from_directory()
     file_names = directory_filenames.get_current_directory_filenames()
     function_connections = []
     for f_name in file_names:
-        function_connections += function_parser.parse_function(f_name, functions_list)
+        function_connections += function_parser.parse_function_2(f_name, functions_list)
 
     g = Digraph('unix', filename='unix.gv',
                 node_attr={'color': 'aquamarine1', 'style': 'filled'})
