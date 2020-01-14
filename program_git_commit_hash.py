9020382a135dedbf4722a97eb7efc11b23c8309f
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2020-01-14 19:48:09 +0100

    updated program_git_commit_hash.py for Story #9

--- program_git_commit_hash.py
+++ program_git_commit_hash.py
@@ -1,4 +1,3 @@
-import os
 from git import Repo
 
 
@@ -9,22 +8,23 @@ def get_git_commit_hash(directory_path):
     try:
         # tworzę obiekt typu Repo aby móc operować na repo wskazywanym przez repo_path
         repo = Repo(repo_path)
-
-        # sprawdzam, czy repozytorium załadowało się poprawnie
-        if not repo.bare:
-
-            # pobieram nazwę aktywnego brancha
-            branch_name = repo.active_branch
-            print('Repozytorium działa na branchu: {}'.format(branch_name))
-
-            commit_hash = list(repo.iter_commits('master'))[0].hexsha
-            print(commit_hash)
-
     except:
         # Jeżeli pliki leżą w folderze nie należącym do repozytorium git, należy użyć wartości "unknown"
         print("Nie można było otworzyć repozytorium pod wskazanym adresem")
         return "unknown"
 
+    # sprawdzam, czy repozytorium załadowało się poprawnie
+    if not repo.bare:
+        # pobieram nazwę aktywnego brancha
+        branch_name = repo.active_branch
+        print('Repozytorium działa na branchu: {}'.format(branch_name))
 
+        # pobieram hash ostatniego commita
+        commit_hash = list(repo.iter_commits('master'))[0].hexsha
+        print('Hash ostatniego commita: {}'.format(commit_hash))
+        return commit_hash
+
+
+# main tylko w celu sprawdzenia działania funkcji, potem trzeba go usunąć
 if __name__ == "__main__":
     get_git_commit_hash(".")
